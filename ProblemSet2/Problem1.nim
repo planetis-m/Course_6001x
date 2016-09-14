3a88680619302af28ffbbbc6d45162ea782e76e3
Antonis G <capoiosct@gmail.com>
2016-09-14 15:13:33 +0300

    Add first problem from pset2

---
+++ ProblemSet2/Problem1.nim
@@ -0,0 +1,23 @@
+# Write a program to calculate the credit card balance after one year 
+# if a person only pays the minimum monthly payment required by the 
+# credit card company each month.
+
+# Needed for round
+import math
+
+let
+    annualInterestRate = 0.2 # annual interest rate as a decimal
+    monthlyPaymentRate = 0.04 # minimum monthly payment rate as a decimal
+    monthlyInterestRate = annualInterestRate / 12.0
+
+# the outstanding balance on the credit card
+var balance = 42.0
+
+for i in 0 .. <12:
+    let minimumPayment = monthlyPaymentRate * balance
+    let unpaidBalance = balance - minimumPayment
+    let interest = monthlyInterestRate * unpaidBalance
+    balance = unpaidBalance + interest
+    echo "Month ", i + 1, " Remaining balance: ", round(balance, 2)
+
+echo "Remaining balance: ", round(balance, 2)
