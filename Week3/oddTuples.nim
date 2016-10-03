b95d0361669d43ba0add0ac59876a300bb57e429
Antonis G <capoiosct@gmail.com>
2016-10-03 23:38:07 +0300

    Update doc for oddArrays

--- Week3/oddTuples.nim
+++ Week3/oddTuples.nim
@@ -1,12 +1,12 @@
 
 proc oddArrays[T](anArray: seq[T]): seq[T] =
     #
-    # aArray: a sequence
+    # anArray: a sequence
     #
-    # returns: sequence, every other element of aArray.
+    # returns: sequence, every other element of anArray.
     #
 
-    # Iterate over the elements in aArray, counting by 2
+    # Iterate over the elements in anArray, counting by 2
     #  (every other element) and adding that element to 
     #  the result
     result = @[]
