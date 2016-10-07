49b97bbe4e8c8833e9a96e5165888a450dc745de
konqoro <capoiosct@gmail.com>
2016-10-07 22:59:56 +0300

    Move to quiz directory

---
+++ Quiz/reverse.nim
@@ -0,0 +1,31 @@
+import algorithm
+
+include display_array
+
+template deep_reverse_private(a: untyped) =
+    #
+    #  assumes L is a list of lists whose elements are ints
+    # Mutates L such that it reverses its elements and also 
+    # reverses the order of the int elements in every element of L. 
+    # It does not return anything.
+    #
+
+    for i in 0 .. a.high:
+        a[i].reverse()
+    a.reverse()
+
+proc deep_reverse*(a: var seq[seq[int]]) =
+    deep_reverse_private(a)
+
+proc deep_reverse*[I, J](a: var array[I, array[J, int]]) =
+    deep_reverse_private(a)
+
+
+when isMainModule:
+    var K = @[@[1, 2], @[3, 4], @[5, 6]]
+    var L = [[1, 2], [3, 4], [5, 6]]
+
+    deep_reverse(K)
+    deep_reverse(L)
+
+    echo K, ' ', L
