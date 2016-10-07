4a7389362ab398dc8cd4957c168bff17bdc818a7
konqoro <capoiosct@gmail.com>
2016-10-07 22:53:22 +0300

    use unittest module

--- temp/deep_reverse.nim
+++ temp/deep_reverse.nim
@@ -1,4 +1,4 @@
-import algorithm
+import algorithm, unittest
 
 include display_array
 
@@ -29,6 +29,25 @@ deep_reverse(L)
 
 echo K, ' ', L
 
-var L = [[0, 1, 2], [1, 2, 3]]
-var M = [[0], [1], [2], [3], [-1]]
-var N = [[0, -1, 2, -3, 4, -5]]
+
+suite "Reversing":
+    test "sequencies":
+        var M = @[@[0, 1, 2], @[1, 2, 3]]
+        var N = @[@[0], @[1], @[2], @[3], @[-1]]
+        var P = @[@[0, -1, 2, -3, 4, -5]]
+        deep_reverse(M)
+        deep_reverse(N)
+        deep_reverse(P)
+        check M == @[@[3, 2, 1], @[2, 1, 0]]
+        check N == @[@[-1], @[3], @[2], @[1], @[0]]
+        check P == @[@[-5, 4, -3, 2, -1, 0]]
+    test "arrays":
+        var M = [[0, 1, 2], [1, 2, 3]]
+        var N = [[0], [1], [2], [3], [-1]]
+        var P = [[0, -1, 2, -3, 4, -5]]
+        deep_reverse(M)
+        deep_reverse(N)
+        deep_reverse(P)
+        check M == [[3, 2, 1], [2, 1, 0]]
+        check N == [[-1], [3], [2], [1], [0]]
+        check P == [[-5, 4, -3, 2, -1, 0]]
