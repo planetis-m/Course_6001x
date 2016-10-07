47815104fecb1d100110c0c77bb9c76c04f3c260
konqoro <capoiosct@gmail.com>
2016-10-07 23:28:58 +0300

    Make use of templates

--- Quiz/reverse_test.nim
+++ Quiz/reverse_test.nim
@@ -1,23 +1,27 @@
 import unittest, reverse
 
+template avoid_typing(a, b: untyped) =
+    var L = a
+    deep_reverse(L)
+    check L == b
+
 suite "Reversing":
     test "sequencies":
-        var M = @[@[0, 1, 2], @[1, 2, 3]]
-        var N = @[@[0], @[1], @[2], @[3], @[-1]]
-        var P = @[@[0, -1, 2, -3, 4, -5]]
-        deep_reverse(M)
-        deep_reverse(N)
-        deep_reverse(P)
-        check M == @[@[3, 2, 1], @[2, 1, 0]]
-        check N == @[@[-1], @[3], @[2], @[1], @[0]]
-        check P == @[@[-5, 4, -3, 2, -1, 0]]
+        avoid_typing(@[@[0, 1, 2], @[1, 2, 3]], 
+                     @[@[3, 2, 1], @[2, 1, 0]])
+
+        avoid_typing(@[@[0], @[1], @[2], @[3], @[-1]],
+                     @[@[-1], @[3], @[2], @[1], @[0]])
+
+        avoid_typing(@[@[0, -1, 2, -3, 4, -5]], 
+                     @[@[-5, 4, -3, 2, -1, 0]])
+
     test "arrays":
-        var M = [[0, 1, 2], [1, 2, 3]]
-        var N = [[0], [1], [2], [3], [-1]]
-        var P = [[0, -1, 2, -3, 4, -5]]
-        deep_reverse(M)
-        deep_reverse(N)
-        deep_reverse(P)
-        check M == [[3, 2, 1], [2, 1, 0]]
-        check N == [[-1], [3], [2], [1], [0]]
-        check P == [[-5, 4, -3, 2, -1, 0]]
+        avoid_typing([[0, 1, 2], [1, 2, 3]], 
+                     [[3, 2, 1], [2, 1, 0]])
+
+        avoid_typing([[0], [1], [2], [3], [-1]],
+                     [[-1], [3], [2], [1], [0]])
+
+        avoid_typing([[0, -1, 2, -3, 4, -5]], 
+                     [[-5, 4, -3, 2, -1, 0]])
