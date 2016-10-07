25720a16087b539239bdebb8c8bf857984e011bc
konqoro <capoiosct@gmail.com>
2016-10-07 22:57:43 +0300

    Split to different file

--- temp/deep_reverse.nim
+++ temp/deep_reverse.nim
@@ -1,4 +1,4 @@
-import algorithm, unittest
+import algorithm
 
 include display_array
 
@@ -14,40 +14,18 @@ template deep_reverse_private(a: untyped) =
         a[i].reverse()
     a.reverse()
 
-proc deep_reverse(a: var seq[seq[int]]) =
+proc deep_reverse*(a: var seq[seq[int]]) =
     deep_reverse_private(a)
 
-proc deep_reverse[I, J](a: var array[I, array[J, int]]) =
+proc deep_reverse*[I, J](a: var array[I, array[J, int]]) =
     deep_reverse_private(a)
 
 
-var K = @[@[1, 2], @[3, 4], @[5, 6]]
-var L = [[1, 2], [3, 4], [5, 6]]
+when isMainModule:
+    var K = @[@[1, 2], @[3, 4], @[5, 6]]
+    var L = [[1, 2], [3, 4], [5, 6]]
 
-deep_reverse(K)
-deep_reverse(L)
+    deep_reverse(K)
+    deep_reverse(L)
 
-echo K, ' ', L
-
-
-suite "Reversing":
-    test "sequencies":
-        var M = @[@[0, 1, 2], @[1, 2, 3]]
-        var N = @[@[0], @[1], @[2], @[3], @[-1]]
-        var P = @[@[0, -1, 2, -3, 4, -5]]
-        deep_reverse(M)
-        deep_reverse(N)
-        deep_reverse(P)
-        check M == @[@[3, 2, 1], @[2, 1, 0]]
-        check N == @[@[-1], @[3], @[2], @[1], @[0]]
-        check P == @[@[-5, 4, -3, 2, -1, 0]]
-    test "arrays":
-        var M = [[0, 1, 2], [1, 2, 3]]
-        var N = [[0], [1], [2], [3], [-1]]
-        var P = [[0, -1, 2, -3, 4, -5]]
-        deep_reverse(M)
-        deep_reverse(N)
-        deep_reverse(P)
-        check M == [[3, 2, 1], [2, 1, 0]]
-        check N == [[-1], [3], [2], [1], [0]]
-        check P == [[-5, 4, -3, 2, -1, 0]]
+    echo K, ' ', L
