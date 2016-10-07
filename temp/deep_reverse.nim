b0dfb2289cb607641fdad9b4e364254ac4e1c23b
konqoro <capoiosct@gmail.com>
2016-10-07 12:47:12 +0300

    Not working for the moment unfortunately

--- temp/deep_reverse.nim
+++ temp/deep_reverse.nim
@@ -1,9 +1,20 @@
-proc deep_reverse(L: var openarray[int]) =
+import algorithm
+
+include display_array
+
+proc deep_reverse[I, J](a: var array[I, array[J, int]]) =
+    #
     #  assumes L is a list of lists whose elements are ints
     # Mutates L such that it reverses its elements and also 
     # reverses the order of the int elements in every element of L. 
     # It does not return anything.
     #
-    for i in L:
-        for j in L[i]:
-            
+
+    for i in a:
+        i.reverse()
+    a.reverse()
+
+
+var L = [[1, 2], [3, 4], [5, 6]]
+deep_reverse(L)
+echo L
