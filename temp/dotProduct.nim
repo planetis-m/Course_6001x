e8f0195150f83c671b03f85a641e2221e274931f
Antonis G <capoiosct@gmail.com>
2016-10-10 23:49:01 +0300

    Simplify code and create and use random arrays

--- temp/dotProduct.nim
+++ temp/dotProduct.nim
@@ -1,21 +1,25 @@
+import random
+
 # Write a function that returns the sum of the pairwise products of listA and listB. 
 # You should assume that listA and listB have the same length and are two lists of 
 # integer numbers. For example, if listA = [1, 2, 3] and listB = [4, 5, 6], the dot 
 # product is 1*4 + 2*5 + 3*6, meaning your function should return: 32
+
+proc randIntList(N: static[int]): array[N, int] =
+    for i in 0 .. <N:
+        let x = random(50)
+        result[i] = x
 
 proc dotProduct(listA, listB: openarray[int]): int =
     #
     # listA: a list of numbers
     # listB: a list of numbers of the same length as listA
     #
-    result = 0
-    let
-        m = len(listA)
-    for i in 0 .. <m:
+    for i in 0 .. high(listA):
         result += listA[i] * listB[i]
 
 let
-    listA = [1, 2, 3]
-    listB = [4, 5, 6]
+    listA = randIntList(100)
+    listB = randIntList(100)
 
 echo dotProduct(listA, listB)
