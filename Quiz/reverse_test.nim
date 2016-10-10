871e15d67934b74ba27529c87d341378b873a44e
Antonis G <capoiosct@gmail.com>
2016-10-10 23:47:38 +0300

    Better name for template

--- Quiz/reverse_test.nim
+++ Quiz/reverse_test.nim
@@ -1,27 +1,27 @@
 import unittest, reverse
 
-template avoid_typing(a, b: untyped) =
-    var L = a
+template are_reversed(a, b: untyped) =
+    new L = a
     deep_reverse(L)
     check L == b
 
 suite "Reversing":
     test "sequencies":
-        avoid_typing(@[@[0, 1, 2], @[1, 2, 3]], 
+        are_reversed(@[@[0, 1, 2], @[1, 2, 3]],
                      @[@[3, 2, 1], @[2, 1, 0]])
 
-        avoid_typing(@[@[0], @[1], @[2], @[3], @[-1]],
+        are_reversed(@[@[0], @[1], @[2], @[3], @[-1]],
                      @[@[-1], @[3], @[2], @[1], @[0]])
 
-        avoid_typing(@[@[0, -1, 2, -3, 4, -5]], 
+        are_reversed(@[@[0, -1, 2, -3, 4, -5]],
                      @[@[-5, 4, -3, 2, -1, 0]])
 
     test "arrays":
-        avoid_typing([[0, 1, 2], [1, 2, 3]], 
+        are_reversed([[0, 1, 2], [1, 2, 3]],
                      [[3, 2, 1], [2, 1, 0]])
 
-        avoid_typing([[0], [1], [2], [3], [-1]],
+        are_reversed([[0], [1], [2], [3], [-1]],
                      [[-1], [3], [2], [1], [0]])
 
-        avoid_typing([[0, -1, 2, -3, 4, -5]], 
+        are_reversed([[0, -1, 2, -3, 4, -5]],
                      [[-5, 4, -3, 2, -1, 0]])
