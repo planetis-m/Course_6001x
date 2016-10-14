05a61cffbba7ab620b504dbaaeab6715d4fc287b
Antonis G <capoiosct@gmail.com>
2016-10-14 21:49:21 +0300

    Removed unused file

--- Week3/display_array.nim
+++
@@ -1,8 +0,0 @@
-
-proc `$`[T](a: openarray[T]): string =
-    ## The `$` operator for arrays.
-    result = "["
-    for val in a:
-        if result.len > 1: result.add(", ")
-        result.add($val)
-    result.add("]")
