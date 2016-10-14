8a65836f577ee6a9b20a910fa8bc6eecadfc9b76
Antonis G <capoiosct@gmail.com>
2016-10-15 00:08:54 +0300

    Use withFile template from manual :-P

--- temp/loadWords.nim
+++ temp/loadWords.nim
@@ -2,15 +2,21 @@ import os, strutils
 
 const lyrics_filename = "lyrics.txt"
 
+template withFile(f, fn, actions: untyped): untyped =
+    var f: File
+    if open(f, fn):
+        try:
+            actions
+        finally:
+            close(f)
+    else:
+        quit("cannot open: " & fn)
+
 proc load_words(): seq[string] =
     result = @[]
 
-    var inFile = open(lyrics_filename)
-    for line in lines(inFile):
-        result.add(line.split({' ', ','}))
-
-    inFile.close()
+    withFile(inFile, lyrics_filename):
+        for line in lines(inFile):
+            result.add(line.split({' ', ','}))
 
 let she_loves_you = load_words()
-
-echo she_loves_you
