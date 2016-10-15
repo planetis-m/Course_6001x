a2cd5880f0312392d267982fb756ca63628c65c9
Antonis G <capoiosct@gmail.com>
2016-10-15 14:30:45 +0300

    Ported to CountTables

--- Week3/songs.nim
+++ Week3/songs.nim
@@ -1,47 +1,58 @@
-import tables
+import tables, sequtils
 
 # Contains the she_loves_you sequence of strings
 include loadWords
 
-proc lyrics_to_frequencies(lyrics: seq[string]): Table[string, int] =
-    result = initTable[string, int]()
+proc lyrics_to_frequencies(lyrics: seq[string]): CountTable[string] =
+    result = initCountTable[string]()
 
     for word in lyrics:
-        if word in result:
-            result[word] += 1
-        else:
-            result[word] = 1
+        result.inc(word)
 
-proc values[A, B](t: Table[A, B]): seq[B] =
+# proc most_common_words(freqs: CountTable[string]): tuple[w: seq[string], b: int] =
+#     let
+#         values = toSeq(freqs.values)
+#         best = max(values)
+#     var words: seq[string] = @[]
+#     echo best
+#     for key in keys(freqs):
+#         if freqs[key] == best:
+#             words.add(key)
+# 
+#     return (words, best)
+# 
+# proc words_often(freqs: var CountTable[string], minTimes: int): seq[tuple[w: seq[string], b: int]] =
+#     result = @[]
+#     var done = false
+#     while not done:
+#         var temp = most_common_words(freqs)
+#         if temp.b >= minTimes:
+#             result.add(temp)
+#             for w in temp.w:
+#                 del(freqs, w)  #remove word from dictionary
+#         else:
+#             done = true
+
+proc words_often(freqs: var CountTable[string], minTimes: int): seq[tuple[w: seq[string], b: int]] =
     result = @[]
 
-    for key in keys(t):
-        result.add(t[key])
+    freqs.sort()
 
-proc most_common_words(freqs: Table[string, int]): tuple[w: seq[string], b: int] =
-    let
-        values = values(freqs)
-        best = max(values)
+    var temp: tuple[w: seq[string], b: int]
+
     var words: seq[string] = @[]
-    echo best
-    for key in keys(freqs):
-        if freqs[key] == best:
-            words.add(key)
+    var previous = largest(freqs).val
 
-    return (words, best)
+    for key, val in pairs(freqs):
+        if val >= minTimes:
+            if val != previous:
+                words = @[key]
+            else:
+                words.add(key)
 
-proc words_often(freqs: var Table[string, int], minTimes: int): seq[tuple[w: seq[string], b: int]] =
-    result = @[]
-    var done = false
-    while not done:
-        var temp = most_common_words(freqs)
-        if temp.b >= minTimes:
+            previous = val
+            temp = (words, previous)
             result.add(temp)
-            for w in temp.w:
-                del(freqs, w)  #remove word from dictionary
-        else:
-            done = true
-
 
 var beatles = lyrics_to_frequencies(she_loves_you)
 echo words_often(beatles, 5)
