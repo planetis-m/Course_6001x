58539140f174c9d6d6ce6105b44b04ed4d8989e1
Antonis G <capoiosct@gmail.com>
2016-10-23 13:55:08 +0300

    Make the output same as the python code

--- Week4/grades.nim
+++ Week4/grades.nim
@@ -4,10 +4,10 @@ proc avg(grades: seq[float]): float =
     assert len(grades) > 0, "No grades data"
     return sum(grades) / len(grades).float
 
-proc get_stats(class_list: Table[seq[string], seq[float]]): Table[seq[string], float] =
-    result = initTable[seq[string], float]()
+proc get_stats(class_list: Table[seq[string], seq[float]]): auto =
+    result = initTable[seq[string], tuple[a: seq[float], b: float]]()
     for name, grades in class_list.pairs():
-        result.add (name, avg(grades))
+        result.add (name, (grades, avg(grades)))
 
 let test_grades = {
     @["peter", "parker"]: @[10.0, 5.0, 85.0],
