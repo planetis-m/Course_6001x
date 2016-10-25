0517bc3daef00df600abc933f5e8dcc2cfae3eb2
Antonis G <capoiosct@gmail.com>
2016-10-26 00:50:07 +0300

    not working

--- Week5/integer_ratio.nim
+++ Week5/integer_ratio.nim
@@ -1,6 +1,6 @@
 import math
 
-proc asIntegerRatio(f: float): tuple[nu, de: int] =
+proc asIntegerRatio(f: float, maxden = 10): tuple[nu, de: int] =
     ## Return a pair of integers, whose ratio is exactly equal to the original
     ## float and with a positive denominator.
     ## Raise OverflowError on infinities and a ValueError on NaNs.
@@ -13,37 +13,14 @@ proc asIntegerRatio(f: float): tuple[nu, de: int] =
     ##   as_integer_ratio(-0.25)
     ##   # (-1, 4)
 
-    var
-        float_part: float
-        exponent: int
-        numerator: int
-        denominator: int
-        p_exponent: int
+    proc number_decimal_places(f: float): int =
+        result = len($f) - 2
 
-    let c = classify(f)
-    if c == fcNan:
-        raise newException(ValueError, "cannot convert NaN to integer ratio")
-    elif c == fcNegInf or c == fcInf:
-        raise newException(OverFlowError, "cannot convert Infinity to integer ratio")
+    let
+        higher = 10^f.number_decimal_places()
+        lower = f.int*higher
+        gcden = gcd(higher, lower)
 
-    float_part = frexp(f, exponent) # f == float_part * 2**exponent exactly
+    result = (round(lower/gcden).int, round(higher/gcden).int)
 
-    for i in 0 .. <300:
-        if float_part == floor(float_part):
-            break
-        float_part *= 2.0
-        dec(exponent)
-
-    numerator = float_part.int
-    denominator = 1
-    p_exponent = abs(exponent)
-
-    if exponent > 0:
-        echo numerator
-        numerator = numerator shl p_exponent
-        echo numerator
-    else:
-        denominator = denominator shl p_exponent
-    return (numerator, denominator)
-
-echo asIntegerRatio(3.2)
+echo asIntegerRatio(3.25)
