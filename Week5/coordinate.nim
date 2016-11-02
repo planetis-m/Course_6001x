bef5bdc46d160c082f0d50e90913fc27ac84af93
Antonis G <capoiosct@gmail.com>
2016-11-02 18:26:20 +0200

    Stuff

--- Week5/coordinate.nim
+++ Week5/coordinate.nim
@@ -4,11 +4,6 @@ type Coordinate = ref object
     x: float
     y: float
 
-let c = Coordinate(x: 3, y: 4)
-
-let origin = new Coordinate
-origin.x = 0
-origin.y = 0
 
 proc distance(a, b: Coordinate): float =
     let x_diff_sq = pow(a.x - b.x, 2)
@@ -19,9 +14,14 @@ proc `$`(c: Coordinate): string =
     "<" & $c.x & ", " & $c.y & ">"
 
 proc `-`(a, b: Coordinate): Coordinate =
-    new(result)
-    result.x = a.x - b.x
-    result.y = a.y - b.y
+    result = Coordinate(x: a.x - b.x, y: a.y - b.y)
+
+
+let c = Coordinate(x: 3, y: 4)
+
+let origin = new Coordinate
+origin.x = 0
+origin.y = 0
 
 echo c.distance(origin)
 echo c
