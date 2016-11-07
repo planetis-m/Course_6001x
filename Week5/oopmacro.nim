05270c13bd4e8d481b5e6e4eb0edf79fcc2845a1
Antonis G <capoiosct@gmail.com>
2016-11-07 12:56:21 +0200

    Added back comments and reintroduced quote

--- Week5/oopmacro.nim
+++ Week5/oopmacro.nim
@@ -1,28 +1,31 @@
 import macros
 
 macro class*(head, body): untyped =
+  # The macro is immediate so that it doesn't
+  # resolve identifiers passed to it
   var typeName, baseName: NimNode
+
   if head.kind == nnkIdent:
+    # `head` is expression `typeName`
     typeName = head
     baseName = ident("RootObj")
   elif head.kind == nnkInfix and $head[0] == "of":
+    # `head` is expression `typeName of baseClass`
     typeName = head[1]
     baseName = head[2]
   else:
     quit "Invalid node: " & head.lispRepr
 
-  result = newStmtList()
-  result.add newNimNode(nnkTypeSection).add(
-    newNimNode(nnkTypeDef).add(
-      typeName,
-      newEmptyNode(),
-      newNimNode(nnkRefTy).add(
-        newNimNode(nnkObjectTy).add(
-          newEmptyNode(),
-          newNimNode(nnkOfInherit).add(baseName),
-          newEmptyNode()))))
+  # create a type section in the result
+  result =
+    quote do:
+      type `typeName` = ref object of `baseName`
 
+  # var declarations will be turned into object fields
   var recList = newNimNode(nnkRecList)
+
+  # Iterate over the statements, adding `this: T`
+  # to the parameters of functions
   for node in body.children:
     case node.kind
     of nnkMethodDef, nnkProcDef:
@@ -36,6 +39,7 @@ macro class*(head, body): untyped =
     else:
       result.add(node)
 
+  # inject recList under objectTy
   result[0][0][2][0][2] = recList
 
 
