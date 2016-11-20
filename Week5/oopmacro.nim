1c2403247c17a9c86062f0c70d8d37579769f0c9
Antonis G <capoiosct@gmail.com>
2016-11-20 23:46:48 +0200

    More modifications for the oopmacro

--- Week5/oopmacro.nim
+++ Week5/oopmacro.nim
@@ -22,13 +22,12 @@ macro class*(head, body): untyped =
 
   # create a type section in the result
   result =
-    if exported:
-      # mark `typeName` with an asterisk
-      quote do:
-        type `typeName`* = ref object of `baseName`
-    else:
-      quote do:
-        type `typeName` = ref object of `baseName`
+    quote do:
+      type `typeName` = ref object of `baseName`
+
+  # mark `typeName` with an asterisk
+  if exported:
+    result[0][0][0] = newNimNode(nnkPostfix).add(ident("*"), typeName)
 
   # var declarations will be turned into object fields
   var recList = newNimNode(nnkRecList)
@@ -36,15 +35,29 @@ macro class*(head, body): untyped =
   # expected name of ctor by convention
   let ctorName = newIdentNode("new" & $typeName)
 
+  template injectThis: untyped =
+    # make sure it is not the ctor proc
+    if node.name.basename != ctorName:
+      # inject `self: T` into the arguments
+      node.params.insert(1, newIdentDefs(ident("self"), typeName))
+
+  template markAsBase: untyped =
+    # find out if user already annotated method with the base pragma
+    let exists = findChild(node.pragma, it.ident == !"base")
+    # add base pragma for methods belonging to a base class
+    if baseName.ident == !"RootObj" and exists.isNil:
+      node.addPragma(ident("base"))
+
   # Iterate over the statements, adding `this: T`
   # to the parameters of functions
   for node in body.children:
     case node.kind
-    of nnkMethodDef, nnkProcDef:
-      # make sure it is not the ctor proc
-      if node.name.basename != ctorName:
-        # inject `self: T` into the arguments
-        node.params.insert(1, newIdentDefs(ident("self"), typeName))
+    of nnkMethodDef:
+      injectThis()
+      markAsBase()
+      result.add(node)
+    of nnkProcDef:
+      injectThis()
       result.add(node)
     of nnkVarSection:
       # variables get turned into fields of the type.
@@ -58,14 +71,15 @@ macro class*(head, body): untyped =
 
 
 when isMainModule:
-  class Animal*(RootObj):
-    var age*: int
+  class Animal(RootObj):
+    var age: int
+    method vocalize = echo "..."
 
   class Person(Animal):
     var name: string
     proc newPerson(name: string, age: int): Person =
       result = Person(name: name, age: age)
-    method vocalize {.base.} = echo "..."
+    method vocalize = echo "Hey"
 
   let john = newPerson("John", 10)
   john.vocalize()
