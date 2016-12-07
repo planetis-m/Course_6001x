32747e64d32da3927a169e029e654dad8b7d4b4d
Antonis G <capoiosct@gmail.com>
2016-12-07 23:55:48 +0200

    Fix it lol

--- Week5/person.nim
+++ Week5/person.nim
@@ -10,10 +10,21 @@ proc setLastName(name: string): string =
   result = captureBetween(name, ' ')
 
 proc setBirthDay(self: var Person, day: string) =
-  self.birthday = times.parse(day, "d/M/yy").toTime
+  self.birthday = times.parse(day, "M/d/yyyy").toTime
 
 proc newPerson(name: string): Person =
   result = Person(name: name, birthday: 0.Time, lastname: setLastName(name))
+
+proc getLastName(self: Person): string =
+  result = self.lastname
+
+proc cmp(a, b: Person): int =
+  if a.lastname == b.lastname:
+    return cmp(a.name, b.name)
+  return cmp(a.lastname, b.lastname)
+
+proc `$`(self: Person): string =
+  result = self.name
 
 # example usage
 
@@ -24,9 +35,9 @@ var
   p4 = newPerson("Andrew Gates")
   p5 = newPerson("Steve Wozniak")
 
-p1.setBirthday("5/14/84")
-p2.setBirthday("3/4/83")
-p3.setBirthday("10/28/55")
+p1.setBirthday("5/14/1984")
+p2.setBirthday("3/4/1983")
+p3.setBirthday("10/28/1955")
 
 
 let personList = [p1, p2, p3, p4, p5]
