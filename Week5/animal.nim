83639f8742ba040223a2c0b0305123b68fd32ae7
Antonis G <capoiosct@gmail.com>
2016-12-09 23:34:14 +0200

    Use .this pragma

--- Week5/animal.nim
+++ Week5/animal.nim
@@ -1,43 +1,45 @@
 import oopmacro
 
+{.this:self.}
+
 class Animal(RootObj):
   var age: int
   var name: string
   proc speak = echo "..."
-  proc get_age: int = self.age
-  proc get_name: string = self.name
-  proc set_age(newage: int) = self.age = newage
-  proc set_name(newname: string) = self.name = newname
-  proc `$`: string = "animal:" & self.name & ":" & $self.age
+  proc get_age: int = age
+  proc get_name: string = name
+  proc set_age(newage: int) = age = newage
+  proc set_name(newname: string) = name = newname
+  proc `$`: string = "animal:" & name & ":" & $age
 
 class Cat(Animal):
   proc newCat(name: string, age: int): Cat =
     result = Cat(name: name, age: age)
   proc speak = echo "Meow"
-  proc `$`: string = "cat:" & self.name & ":" & $self.age
+  proc `$`: string = "cat:" & name & ":" & $age
 
 class Rabbit(Animal):
   proc newRabbit(name: string, age: int): Rabbit =
     result = Rabbit(name: name, age: age)
   proc speak = echo "Meep"
-  proc `$`: string = "rabbit:" & self.name & ":" & $self.age
+  proc `$`: string = "rabbit:" & name & ":" & $age
 
 class Person(Animal):
   proc newPerson(name: string, age: int): Person =
     result = Person(name: name, age: age, friends: @[])
   var friends: seq[Person]
   proc speak = echo "Hello"
-  proc get_friends: seq[Person] = self.friends
+  proc get_friends: seq[Person] = friends
   proc add_friend(friend: Person) =
-    if friend notin self.friends:
-      self.friends.add(friend)
+    if friend notin friends:
+      friends.add(friend)
   proc age_diff(other: Person) =
-    let diff = self.age - other.age
-    if self.age > other.age:
-      echo self.name, " is ", diff, " years older than ", other.name
+    let diff = age - other.age
+    if age > other.age:
+      echo name, " is ", diff, " years older than ", other.name
     else:
-      echo self.name, " is ", -diff, " years younger than ", other.name
-  proc `$`: string = "person:" & self.name & ":" & $self.age
+      echo name, " is ", -diff, " years younger than ", other.name
+  proc `$`: string = "person:" & name & ":" & $age
 
 
 let c = Cat(name: "Tom")
