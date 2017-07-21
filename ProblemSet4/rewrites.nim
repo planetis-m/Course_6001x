bf0bccba8008bce43db6760a9f4ad790e8316081
Antonis G <capoiosct@gmail.com>
2017-07-21 18:51:47 +0300

    Remove overload that works (tested)

--- ProblemSet4/rewrites.nim
+++ ProblemSet4/rewrites.nim
@@ -2,21 +2,13 @@
 # Redefinitions
 # (used to overload procedures)
 
-import algorithm, sequtils
+import algorithm
 
 proc contains(a: seq[string], item: string): bool {.inline.} =
     # in operator calls the procedure contains, which
     # in its turn is a shortcut to find(a, item) >= 0
     # overload contains to use binary search for wordlist which is ordered
     return binarySearch(a, item) >= 0
-
-proc `==`[A](s, t: CountTable[A]): bool =
-    let seq1 = toSeq(s.pairs)
-    let seq2 = toSeq(t.pairs)
-    if len(seq1) == len(seq2):
-        for i in seq1:
-            if i notin seq2: return false
-        return true
 
 proc toCountTable[A](pairs: openArray[(A, int)]): CountTable[A] =
     ## creates a new hash table that contains the given `pairs`.
