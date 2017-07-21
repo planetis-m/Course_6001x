c69a48c6c966f914d473a5fc1c4ded661d36b572
Antonis G <capoiosct@gmail.com>
2017-07-21 15:09:31 +0300

    Remove comment

--- Week2/polysum.nim
+++ Week2/polysum.nim
@@ -1,12 +1,12 @@
 import math
 
 proc polysum(n, s: float): float =
-    # 
+    #
     # Input : n, s - positive integers where `n` is the number of sides and `s`
     # is the length of each side.
-    # 
+    #
     # Returns the sum of the perimeter squared and the area.
-    # 
+    #
 
     # This variable is the total area of the polygon.
     let area = 0.25 * n * pow(s, 2) / tan(PI / n)
@@ -16,8 +16,6 @@ proc polysum(n, s: float): float =
 
     return round(pow(perimeter, 2) + area, 4)
 
-# Not working
-#let t = 0.25 * 6^2
 
 echo polysum(4, 90)
 echo polysum(14, 79)
