cdc3a4566284727dee3d9bcd50e2352619502464
Antonis G <capoiosct@gmail.com>
2018-02-08 18:21:46 +0200

    and one more

---
+++ Week4/getnum.nim
@@ -0,0 +1,10 @@
+import rdstdin, strutils
+
+while true:
+   try:
+      let inp = readLineFromStdin("Please enter an integer: ")
+      let num = parseInt(inp)
+      break
+   except ValueError:
+      echo("Input is not an integer; try again")
+echo("Correct input of an integer")
