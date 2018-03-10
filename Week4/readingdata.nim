50d6a0b581c8eff381ffcdc12cfe8ffb6137582a
Antonis G <capoiosct@gmail.com>
2018-03-10 15:32:32 +0200

    renovate

--- Week4/readingdata.nim
+++ Week4/readingdata.nim
@@ -4,19 +4,19 @@ let file_name = readLineFromStdin("Provide a name of a file of data: ")
 
 var data: seq[seq[string]] = @[]
 var fh: File
-var succeeded = true
+var succeeded: bool
 
 try:
    fh = open(file_name, fmRead)
+   succeeded = true
+   for line in lines(fh):
+      if line != "\n":
+         let it = line.strip.split(", ") #remove trailing \n
+         data.add(it)
 except IOError:
-   succeeded = false
    echo("cannot open ", file_name)
 finally:
    if succeeded:
-      for line in lines(fh):
-         if line != "\n":
-            let it = line.strip.split(", ") #remove trailing \n
-            data.add(it)
       close(fh)
 
 var gradesData: seq[(seq[string], seq[int])] = @[]
