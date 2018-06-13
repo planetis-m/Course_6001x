f7d523997b8060c201ebfaa4466fe01b355413f0
Antonis G <capoiosct@gmail.com>
2018-06-13 16:28:44 +0300

    new version

--- Week5/oopmacro.nim
+++ Week5/oopmacro.nim
@@ -1,68 +1,65 @@
 import macros
 
+type
+   ClassBuilder = ref object
+      # Holds the class context 
+      typeName, baseName, recList: NimNode
+      ctorName: string
+
+proc transform(node: NimNode, b: ClassBuilder): NimNode =
+   case node.kind
+   of nnkMethodDef, nnkProcDef:
+      if node.name.kind != nnkAccQuoted and eqIdent(b.ctorName, $node.name.basename):
+         node.params[0] = b.typeName
+      else:
+         node.params.insert(1, newIdentDefs(ident("self"), b.typeName))
+      result = node
+   of nnkVarSection:
+      node.copyChildrenTo(b.recList)
+      # need to return None node 
+      result = newNimNode(nnkNone)
+   else:
+      result = copyNimNode(node)
+      for n in node:
+         let x = transform(n, b)
+         # filter out None node kinds 
+         if x.kind != nnkNone: result.add(x)
+
 macro class*(head, body): untyped =
-   # The absence of typed parameters make this macro immediate,
-   # it doesn't resolve identifiers passed to it.
-   var typeName, baseName: NimNode
+   let b = ClassBuilder()
 
-   # flag if object should be exported
-   var isExported: bool
-
+   var isExported = false
    if head.kind == nnkCall:
-      # `head` is expression `typeName(baseClass)`
-      typeName = head[0]
-      baseName = head[1]
+      b.typeName = head[0]
+      b.baseName = head[1]
    elif head.kind == nnkInfix and $head[0] == "*" and head[2].kind == nnkPar:
-      # `head` is expression `typeName*(baseClass)`
-      typeName = head[1]
-      baseName = head[2][0]
+      b.typeName = head[1]
+      b.baseName = head[2][0]
       isExported = true
    else:
       error "Invalid node: " & head.lispRepr
 
-   # create a new stmtList for the result
-   result = newStmtList()
-
-   # create a type section in the result
    template declare(a, b): untyped =
       type a = ref object of b
 
    template declarePub(a, b): untyped =
       type a* = ref object of b
 
-   if isExported:
-      result.add getAst(declarePub(typeName, baseName))
-   else:
-      result.add getAst(declare(typeName, baseName))
-
-   # var declarations will be turned into object fields
-   var recList = newNimNode(nnkRecList)
-
-   # expected name of ctor by convention
-   let ctorName = "new" & $typeName
-
-   # Iterate over the statements, adding `self: T`
-   # to the parameters of functions
-   for node in body.children:
-      case node.kind
-      of nnkMethodDef, nnkProcDef:
-         # check if it is the ctor proc
-         if node.name.kind != nnkAccQuoted and eqIdent(ctorName, $node.name.basename):
-            # specify the return type of the ctor proc
-            node.params[0] = typeName
-         else:
-            # inject `self: T` into the arguments
-            node.params.insert(1, newIdentDefs(ident("self"), typeName))
-         result.add(node)
-      of nnkVarSection:
-         # variables get turned into fields of the type.
-         node.copyChildrenTo(recList)
+   let typeDecl =
+      if isExported:
+         getAst(declarePub(b.typeName, b.baseName))
       else:
-         result.add(node)
+         getAst(declare(b.typeName, b.baseName))
 
-   # inject recList under objectTy
-   result[0][0][2][0][2] = recList
+   b.recList = newNimNode(nnkRecList)
+   # a NimNode is a reference type so this is possible
+   typeDecl[0][2][0][2] = b.recList
 
+   b.ctorName = "new" & $b.typeName
+
+   # this alias is modified by the transform proc
+   result = newStmtList(typeDecl, transform(body, b))
+   # echo result.treeRepr
 
 when isMainModule:
    class Animal(RootObj):
