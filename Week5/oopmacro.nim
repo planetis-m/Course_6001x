0095953fd3c89b20e13758490fb71d9c601719b2
Antonis G <capoiosct@gmail.com>
2018-06-13 16:51:10 +0300

    newfeature

--- Week5/oopmacro.nim
+++ Week5/oopmacro.nim
@@ -6,15 +6,33 @@ type
       typeName, baseName, recList: NimNode
       ctorName: string
 
+proc insertSelf(node: NimNode, b: ClassBuilder): NimNode =
+   case node.kind
+   of nnkIdent:
+      for r in b.recList:
+         for i in 0 .. r.len - 3:
+            if eqIdent($node, $r[i]):
+               return newDotExpr(ident("self"), node)
+      result = node
+   else:
+      result = copyNimNode(node)
+      for n in node:
+         result.add insertSelf(n, b)
+
 proc transform(node: NimNode, b: ClassBuilder): NimNode =
    case node.kind
    of nnkMethodDef, nnkProcDef:
+      # check if it is the ctor proc
       if node.name.kind != nnkAccQuoted and eqIdent(b.ctorName, $node.name.basename):
          node.params[0] = b.typeName
+         result = node
       else:
+         # inject `self: T` into the arguments
          node.params.insert(1, newIdentDefs(ident("self"), b.typeName))
-      result = node
+         # automatic self insertion for object fields
+         result = insertSelf(node, b)
    of nnkVarSection:
+      # variables get turned into fields of the type.
       node.copyChildrenTo(b.recList)
       # need to return None node 
       result = newNimNode(nnkNone)
@@ -22,16 +40,18 @@ proc transform(node: NimNode, b: ClassBuilder): NimNode =
       result = copyNimNode(node)
       for n in node:
          let x = transform(n, b)
-         # filter out None node kinds 
+         # filter out None node kinds
          if x.kind != nnkNone: result.add(x)
 
 macro class*(head, body): untyped =
    let b = ClassBuilder()
-
+   # flag if object should be exported
    var isExported = false
+   # `head` is expression `typeName(baseClass)`
    if head.kind == nnkCall:
       b.typeName = head[0]
       b.baseName = head[1]
+   # `head` is expression `typeName*(baseClass)`
    elif head.kind == nnkInfix and $head[0] == "*" and head[2].kind == nnkPar:
       b.typeName = head[1]
       b.baseName = head[2][0]
@@ -45,19 +65,20 @@ macro class*(head, body): untyped =
    template declarePub(a, b): untyped =
       type a* = ref object of b
 
+   # create a type section in the result
    let typeDecl =
       if isExported:
          getAst(declarePub(b.typeName, b.baseName))
       else:
          getAst(declare(b.typeName, b.baseName))
 
+   # expected name of ctor by convention
+   b.ctorName = "new" & $b.typeName
+   # var declarations will be turned into object fields
    b.recList = newNimNode(nnkRecList)
-   # a NimNode is a reference type so this is possible
+   # inject recList under objectTy
    typeDecl[0][2][0][2] = b.recList
 
-   b.ctorName = "new" & $b.typeName
-
-   # this alias is modified by the transform proc
    result = newStmtList(typeDecl, transform(body, b))
    # echo result.treeRepr
 
@@ -65,14 +86,14 @@ when isMainModule:
    class Animal(RootObj):
       var age: int
       method vocalize {.base.} = echo "..."
-      proc `$`: string = "animal:" & $self.age
+      proc `$`: string = "animal:" & $age
 
    class Person(Animal):
       var name: string
       proc newPerson(name: string, age: int) =
          result = Person(name: name, age: age)
       method vocalize = echo "Hey"
-      proc `$`: string = "person:" & self.name & ":" & $self.age
+      proc `$`: string = "person:" & name & ":" & $self.age
 
    let john = newPerson("John", 10)
    john.vocalize()
